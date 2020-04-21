e849cf51863a4514cc773e0174f1e162c1b61464
Michael <michaellenehan1gmail.com>
2020-04-21 09:00:49 +0100

    Completed Assignment

--- Assignment/Assignment2/code/Q6/q6.m
+++ Assignment/Assignment2/code/Q6/q6.m
@@ -1,41 +1,74 @@
-addpath(".")
-
-function retval = erlangB (N, A, Ex)
-	retval = (A*Ex)/(N+A*Ex);
-endfunction
-
-function retval = mfunc (A, Ex)
-	retval = A*Ex;
-endfunction
-
-function retval = var (M, A, N)
-	retval = M*(1-M+A/(N+1-A+M));
-endfunction
-
 AmicA = 20;
 AmicB = 30;
 AmicC = 25;
 Amac = 5;
-B = 0;
-Ex = 1;
-E1 = E2 = E3 = Ex
+B = 0.01;
 Nmic=0;
 Nmac=0;
+MacCost = 3;
+MicCost = 1;
+TotalCost = zeros(0,1000)
+cnt = 1;
 
-while(B<= 0.01)
-	M1 = mfunc(AmicA, Ex)
-	V1 = var(M1, AmicA, Nmic)
+f1 = fopen('nVals.txt', 'w');
+f2 = fopen('costs.txt', 'w');
 
-	M2 = mfunc(AmicB, Ex)
-	V2 = var(M2, AmicB, Nmic)
+while(Nmic <= 100)
+	E1 = E2 = E3 = 1;
+	Ex = 1;
+	x=0;
+	i=0;
+	tmp = Nmac;
+	while(x <= Nmic)
+		E1 = (AmicA*E1)/(x+AmicA*E1);
+		E2 = (AmicB*E2)/(x+AmicB*E2);
+		E3 = (AmicC*E3)/(x+AmicC*E3);
+		x++;
+	endwhile
 
-	M3 = mfunc(AmicC, Ex)
-	V3 = var(M3, AmicC, Nmic)
+	M1 = AmicA*E1;
+	V1 = M1*(1-M1+1*(AmicA/(Nmic+1-AmicA+M1)));
 
-	M = M1+M2+M3
-	V = V1+V2+V3
-	Z = V/M
-	Astar = V+3*Z(Z-1)
-	Nstar = Astar*((M+Z)/(M+Z-1))-M-1
-	B = erlangB(Nmac, Astar, Ex)
+	M2 = AmicB*E2;
+	V2 = M2*(1-M2+1*(AmicB/(Nmic+1-AmicB+M2)));
+
+	M3 = AmicC*E3;
+	V3 = M3*(1-M3+1*(AmicC/(Nmic+1-AmicC+M3)));
+
+	while(Ex > B)
+		E4 = 1;
+		while(i <= Nmac)
+			E4 = (Amac*E4)/(i+Amac*E4);
+			M4 = Amac*E4;
+			V4 = M4*(1-M4+1*(Amac/(i+1-Amac+M4)));
+
+			M = M1+M2+M3+M4;
+			V = V1+V2+V3+V4;
+			Z = V/M;
+			Astar = V+3*Z*(Z-1);
+			Nstar = Astar*((M+Z)/(M+Z-1))-M-1;
+			Ex = (Astar*Ex)/(i+Nstar+Astar*Ex);
+			i++;
+		endwhile
+		Nmac++;
+	endwhile
+	fprintf(f1, "Nmic: %d \t Nmac: %d \t Astar: %d \t Ex: %d\n", Nmic, Nmac,
+	Astar, Ex);
+	TotalCost(cnt) = (MacCost*Nmac) + (MicCost*Nmic);
+	fprintf(f2, "Mic Cost: %d \t Mac Cost: %d \t Total: %d\n", MicCost*Nmic,
+	MacCost*Nmac, TotalCost(cnt));
+	cnt++;
+	Nmac = tmp;
+	Nmic++;
+	Nmac++;
 endwhile
+
+h = figure();
+plot(0:1:100, TotalCost);
+xlabel("Mic Channels");
+ylabel("Total System Cost");
+title("Total System Cost");
+print(h, "Q6.pdf", "-dpdflatexstandalone");
+system("pdflatex Q6.pdf")
+system("pdftoppm Q6.pdf Q6 -png");
+pause();
